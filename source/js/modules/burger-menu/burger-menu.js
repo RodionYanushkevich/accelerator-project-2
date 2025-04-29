bf5b41f2afe36882e299320f066f18366ecb12dc
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2025-04-29 20:53:15 +0700

    save

--- source/js/modules/burger-menu/burger-menu.js
+++ source/js/modules/burger-menu/burger-menu.js
@@ -1,7 +1,20 @@
 const button = document.querySelector('.header__button');
 const header = document.querySelector('.header');
+const navigationLinks = document.querySelectorAll('.navigation__link');
+
 
 button.addEventListener('click', ()=>{
   header.classList.toggle('header--menu-shown');
   button.classList.toggle('header__button--cross');
+
+  if (!button.classList.contains('header__button--cross') & window.innerWidth < 1440) {
+    navigationLinks.forEach((link) => {
+      link.setAttribute('tabindex', '-1');
+    });
+  } else {
+    navigationLinks.forEach((link) => {
+      link.setAttribute('tabindex', '0');
+    });
+  }
 });
+
