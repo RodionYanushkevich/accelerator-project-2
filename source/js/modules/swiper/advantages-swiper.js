a9fa41fccb9983dcf3fa0275c87c1133c756b160
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2025-05-03 18:11:10 +0700

    save

--- source/js/modules/swiper/advantages-swiper.js
+++ source/js/modules/swiper/advantages-swiper.js
@@ -14,28 +14,94 @@ const prevButton = document.querySelector('.advantages__swiper-button.swiper-but
 
 const Slides = [];
 
+// const initSwiper = () => {
+//   advantagesSwiper = new Swiper('.advantages__swiper', {
+//     modules: [Navigation],
+//     loop:true,
+//     speed: 300,
+//     spaceBetween: 30,
+//     slidesPerView: 3,
+//     // slidesPerGroup:2,
+//     centeredSlides: true,
+//     initialSlide: 2,
+//     // allowTouchMove: false,
+//     // simulateTouch: false,
+//     on: {
+//       init: function() {
+//         this.slides.forEach((slide) => {
+//           Slides.push(slide.cloneNode(true));
+
+//         });
+//       }},
+//     navigation: {
+//       nextEl: '.advantages__swiper-button.swiper-button-next',
+//       prevEl: '.advantages__swiper-button.swiper-button-prev',
+//     },
+//   });
+// };
+
+const advantagesList = document.querySelector('.advantages__list');
+const slides = document.querySelectorAll('.advantages__list-item');
+
 const initSwiper = () => {
   advantagesSwiper = new Swiper('.advantages__swiper', {
     modules: [Navigation],
-    loop:true,
-    speed: 300,
+    slidesPerView: 'auto',
+    loop: true,
     spaceBetween: 30,
-    slidesPerView: 3,
     centeredSlides: true,
+    slidesPerGroup: 2,
     initialSlide: 2,
-    on: {
-      init: function() {
-        this.slides.forEach((slide) => {
-          Slides.push(slide.cloneNode(true));
+    on: { init: function() {
+      slides.forEach((slide) => {
+        const clone = slide.cloneNode(true);
+        clone.classList.Add('slide-clone');
+        advantagesList.appendChild(clone);
+      });
 
-        });
-      }},
+    } },
     navigation: {
       nextEl: '.advantages__swiper-button.swiper-button-next',
       prevEl: '.advantages__swiper-button.swiper-button-prev',
     },
   });
 };
+const i = 0;
+
+
+// nextButton.addEventListener ('click', ()=> {
+//   const wrapper = advantagesSwiper.wrapperEl;
+//   const active = advantagesSwiper.activeIndex ;
+
+//   const slides = advantagesSwiper.slides;
+//   const clone1 = Slides[active - 1].cloneNode(true);
+//   const clone = Slides[active - 2].cloneNode(true);
+//   wrapper.appendChild(clone);
+//   wrapper.appendChild(clone1);
+//   advantagesSwiper.slideTo(active + 2);
+//   i = i + 1;
+//   if (i >= 2) {
+//     slides[0].remove();
+//     slides[1].remove();
+//   }
+//   advantagesSwiper.update();
+//   clone.style.color = 'red';
+//   clone1.style.color = 'green';
+
+
+//   // advantagesSwiper.slideTo(active);
+
+//   // setTimeout(() => {
+//   // }, 1000);
+//   // advantagesSwiper.update();
+// });
+
+// prevButton.addEventListener ('click', ()=> {
+//   const active = advantagesSwiper.activeIndex ;
+
+//   console.log(active);
+
+// });
 
 const destroySwiper = () => {
   if (advantagesSwiper) {
@@ -49,62 +115,12 @@ const resizeListener = () => {
   if (window.innerWidth >= 1440 && !advantagesSwiper) {
     swiperClassListToggle(advantagesSwiperContainer);
     initSwiper();
+
   } else if (window.innerWidth < 1440 && advantagesSwiper) {
     destroySwiper();
   }
 };
 
-let i = 0;
-
-nextButton.addEventListener ('click', ()=> {
-  const wrapper = advantagesSwiper.wrapperEl;
-  const slides = advantagesSwiper.slides;
-  const active = advantagesSwiper.activeIndex ;
-
-  const clone = Slides[i].cloneNode(true);
-  i = i + 1;
-  // Убрись за собой потом
-
-  clone.style.opacity = '0.5';
-  wrapper.appendChild(clone);
-
-  if (i === 5) {
-    i = 0;
-  }
-
-  advantagesSwiper.slideTo(active - 1, 0, false);
-
-  advantagesSwiper.update();
-  slides[0].remove();
-});
-
-
-prevButton.addEventListener ('click', ()=> {
-  const wrapper = advantagesSwiper.wrapperEl;
-  const slides = advantagesSwiper.slides;
-  const active = advantagesSwiper.activeIndex ;
-  if (i === 0) {
-    i = 5;
-  }
-
-  i = i - 1;
-
-  const clone = Slides[i].cloneNode(true);
-
-
-  clone.style.opacity = '0.5';
-  // wrapper.appendChild(clone);
-  wrapper.insertBefore(clone, wrapper.firstChild);
-
-  if (i === 0) {
-    i = 5;
-  }
-
-  advantagesSwiper.slideTo(active + 1, 0, false);
-
-  slides[slides.length - 1].remove();
-  advantagesSwiper.update();
-});
 
 window.addEventListener('resize', resizeListener);
 resizeListener();
